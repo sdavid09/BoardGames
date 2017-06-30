51f5ee148d2366c35302a97121187d22fed9dcbc
Shobin David <SDavid@efji.com>
2017-06-30 10:31:25 -0500

    Added Grid

--- board.py
+++ board.py
@@ -3,15 +3,32 @@
 class Board(object):
 
     def __init__(self, xsize, ysize):
-        self.xsize = xsize
-        self.ysize = ysize
+        self.xsize = xsize + 1
+        self.ysize = ysize + 1
         self.board = []
         self.setupBoard()
 
     def setupBoard(self):
+        """Setup Board"""
         for i in range( 0, self.ysize):
             self.board.append(["0"]*self.xsize)
+        self.createGrid()
+        self.board[0][0] = ' '
+
+    def createGrid(self):
+        """Create a Grid Reference Row are Alphabets and Columns by Number"""
+        k = 0
+        for i in range(1, self.xsize):
+            self.board[0][i] = chr( ord('a') + k)
+            k+=1
+        for i in range(1, self.ysize):
+            self.board[i][0] = str(i)
+
+    def setBoardPieces(self): 
+        print "Board:"
+        #self.board[3][4] = u'\u2654'
 
     def displayBoard(self):
+        """Print out Board"""
         for row in self.board:
             print " ".join(row)
--- chessboard.py
+++ chessboard.py
@@ -5,5 +5,5 @@ class ChessBoard(Board):
     def __init__(self, xsize, ysize):
         super(ChessBoard, self).__init__(xsize, ysize)
 
-chess = ChessBoard(8,8)
+chess = ChessBoard(9,8)
 chess.displayBoard()
