522a02e2cc3bcf67cc28230216e042253beb7ec8
Shobin <shobin.david9@gmail.com>
2020-06-05 00:23:32 -0500

    added more grid tests and changes

--- src/Board/Grid.py
+++ src/Board/Grid.py
@@ -2,14 +2,17 @@
 class Grid:
     """ Creates a 2D array specified by row and column """
 
-    def __init__(self, X_SIZE, Y_SIZE):
-        self.grid = [["*" for x in range(X_SIZE)] for y in range(Y_SIZE)]
+    def __init__(self, X_SIZE, Y_SIZE, item=""):
+        self._grid = [[item for x in range(X_SIZE)] for y in range(Y_SIZE)]
 
     def __len__(self):
-        return sum([len(element) for element in self.grid])
+        return sum([len(element) for element in self._grid])
 
     def __getitem__(self, position):
-            return self.grid[position]
+        return self._grid[position]
+
+    def __setitem__(self, index, value):
+        self._grid[index] = value
 
     def __repr__(self):
-        return str(self.grid)
+        return str(self._grid)
--- test/src/Board/Grid_Test.py
+++ test/src/Board/Grid_Test.py
@@ -17,13 +17,29 @@ class GridTest(unittest.TestCase):
 
     def test_get_item(self):
         # get value from 2d array
-        grid = Grid(8, 8)
+        grid = Grid(8, 8, "*")
         self.assertEqual(grid[0][0], "*")
+
+    def test_get_entire_row(self):
+        # get entire row of grid
+        grid = Grid(8, 8, "*")
+        self.assertEqual(grid[0], ["*","*", "*", "*", "*", "*", "*", "*"])
 
     def test_set_item(self):
         # assign value to a 2d array
         grid = Grid(8, 8)
         grid[0][0] = 9
         self.assertEqual(grid[0][0], 9)
+
+    def test_set_entire_row(self):
+        # get entire row of grid
+        grid = Grid(8, 8, "*")
+        grid[0]= ["X","X", "*", "*", "X", "X", "X", "*"]
+        self.assertEqual(grid[0], ["X","X", "*", "*", "X", "X", "X", "*"])
+
+    def test_get_entire_board(self):
+        # get entire row of grid
+        grid = Grid(3, 3, "*")
+        self.assertEqual(str(grid), "[['*', '*', '*'], ['*', '*', '*'], ['*', '*', '*']]")
 
 
