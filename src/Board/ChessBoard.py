131e1b851e75ca578d2d65dd748b3fcdaede2387
Shobin <shobin.david9@gmail.com>
2020-06-05 14:31:24 -0500

    added more chess  board logic

--- src/Board/ChessBoard.py
+++ src/Board/ChessBoard.py
@@ -1,30 +1,58 @@
 from .Grid import Grid
 
 whitepieces = {
-    'WKing'   : u'\u2654',
-    'WQueen'  : u'\u2655',
-    'WRook'   : u'\u2656',
-    'WBishop' : u'\u2657',
-    'WKnight' : u'\u2658',
-    'WPawn'   : u'\u2659',
+    'King'   : u'\u2654',
+    'Queen'  : u'\u2655',
+    'Rook'   : u'\u2656',
+    'Bishop' : u'\u2657',
+    'Knight' : u'\u2658',
+    'Pawn'   : u'\u2659',
 }
 
 blackpieces = {
-    'BKing'   : u'\u265A',
-    'BQueen'  : u'\u265B',
-    'BRook'   : u'\u265C',
-    'BBishop' : u'\u265D',
-    'BKnight' : u'\u265E',
-    'BPawn'   : u'\u265F',
+    'King'   : u'\u265A',
+    'Queen'  : u'\u265B',
+    'Rook'   : u'\u265C',
+    'Bishop' : u'\u265D',
+    'Knight' : u'\u265E',
+    'Pawn'   : u'\u265F',
 }
 
 class ChessBoard:
     def __init__(self):
         self.board = Grid(8,8)
+        self.row ={'A': 1, 'B': 2, 'C': 3,
+                   'D': 4, 'E': 5, 'F': 6,
+                   'G': 7, 'H': 8}
 
     def __len__(self):
         return len(self.board)
 
     def __repr__(self):
         return repr(self.board)
+
+    def __setitem__(self, position, value):
+        row, col = self.check_chess_notation(position)
+        if row and col:
+            self.board[self.row[row.upper()]][int(col)] = value
+        else:
+            self.board[position] = value
+
+    def __getitem__(self, position):
+        row, col = self.check_chess_notation(position)
+        if row and col:
+            return self.board[self.row[row.upper()]][int(col)]
+        else:
+            return self.board[position]
+
+    def check_chess_notation(self, position):
+        """ Check if first character is letter, Second is  number - 1 and greater then 0 """
+        row, col = '', ''
+        if len(str(position)) == 2 and position[0].isalpha() \
+                                   and int(position[1]) \
+                                   and int(position[1]) >= 1 \
+                                   and int(position[1]) - 1 >= 0:
+            row, col = position
+        return row, col
+
 
--- test/src/Board/ChessBoard_Test.py
+++ test/src/Board/ChessBoard_Test.py
@@ -7,3 +7,21 @@ class ChessBoardTest(unittest.TestCase):
         # check if board is a 8x8, 64 pieces
         chess_board = ChessBoard()
         self.assertEqual(len(chess_board), 64)
+
+    def test_invalid__chess_notation(self):
+        chess_board = ChessBoard()
+        row, col = chess_board.check_chess_notation('ab11')
+        self.assertEqual(row, '')
+        self.assertEqual(col, '')
+
+    def test_valid__chess_notation(self):
+        chess_board = ChessBoard()
+        row, col = chess_board.check_chess_notation('a1')
+        self.assertEqual(row, 'a')
+        self.assertEqual(col, '1')
+
+    def test_set_and_get_item_by_chess_notation(self):
+        chess_board = ChessBoard()
+        king =   u'\u265A'
+        chess_board['a1'] = king
+        self.assertEqual(chess_board['a1'], king)
