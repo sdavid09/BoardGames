b340900e4be21ac9b0f7e86a0b2acae935e7423e
Shobin <shobin.david9@gmail.com>
2020-07-22 21:31:24 -0500

    fixed initial pieces display

--- src/Site/chess/static/chess/chess.js
+++ src/Site/chess/static/chess/chess.js
@@ -1,42 +1,39 @@
 import { Piece, Rook, Knight, Bishop, Queen, King, Pawn } from "./piece.js"
 
-
-function addFirstPlayerPieces() {
-    let piece_row = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1']
-    let pieces = ['\u2656', '\u2658', '\u2657', '\u2655', '\u2654', '\u2657','\u2658', '\u2656']
-    piece_row.forEach((element, index)=>{
-        let piece = document.querySelector(`#${element}`)
-        piece.classList.add("player-one-pieces")
-        piece.textContent=`${pieces[index]}`
-    })
-
-    let pawns_row = ['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2']
-    pawns_row.forEach(element => {
-       let pawn = document.querySelector(`#${element}`)
-        pawn.classList.add("player-one-pieces")
-        pawn.textContent = '\u2659';
-    });
-}
-function addSecondPlayerPieces() {
-    let piece_row = ['A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8']
-    let pieces = ['\u265C', '\u265E', '\u265D', '\u265B', '\u265A', '\u265D','\u265E', '\u265C']
-    piece_row.forEach((element, index)=>{
-       let piece = document.querySelector(`#${element}`)
-        piece.classList.add("player-two-pieces")
-        piece.textContent=`${pieces[index]}`
-    })
-
-    let pawns_row = ['A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7']
-    pawns_row.forEach(element => {
-      let pawn = document.querySelector(`#${element}`)
-        pawn.classList.add("player-two-pieces")
-        pawn.textContent = '\u265F';
-    });
-}
-pieces.forEach((element)=>{
-    let square = document.querySelector(`#${element.square}`)
-    square.textContent=`${element.icon}`
+pieces.forEach(element => {
+    let boardSquare = document.querySelector(`#${element.square}`)
+    boardSquare.innerHTML=`<span>${element.icon}</span>`;
 });
+
+let endpoint = window.location.protocol == 'https:' ? 'wss://' : 'ws://' + window.location.host + window.location.pathname
+let socket = new WebSocket(endpoint);
+socket.onopen= function(e){
+    console.log("open", e)
+}
+socket.onmessage = function(e){
+    console.log("message", e);
+
+}
+let table = document.querySelector("table")
+table.addEventListener("click", function(event){
+    console.log("CLICKED")
+    if(event.target.closest('td')){
+
+        let board = {'message': event.target.textContent}
+        socket.send(JSON.stringify(board))
+
+}
+});
+
+
+socket.onerror= function(e){
+    console.log("error", e)
+}
+
+socket.onclose= function(e){
+    console.log("close", e)
+}
+
 // addFirstPlayerPieces();
 // addSecondPlayerPieces()
 
