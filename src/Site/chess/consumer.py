1c1e52d58262b7fe2e1359a1122044d6003c286c
Shobin <shobin.david9@gmail.com>
2020-07-24 17:40:56 -0500

    added sync movement

--- src/Site/chess/consumer.py
+++ src/Site/chess/consumer.py
@@ -1,4 +1,5 @@
 import asyncio
+from asgiref.sync import sync_to_async
 import json
 from channels.consumer import AsyncConsumer
 from channels.generic.websocket import WebsocketConsumer, JsonWebsocketConsumer
@@ -7,21 +8,42 @@ class GameConsumer(AsyncConsumer):
     http_user=True
     async def websocket_connect(self, event):
         print("Connected", event)
+        game = "first"
+        self.game = game
+        await self.channel_layer.group_add(
+            game,
+            self.channel_name
+        )
         await self.send({
             "type": "websocket.accept"
         })
 
     async def websocket_receive(self, event):
         msg = json.loads(event['text'])
-        print("Receive", msg["message"])
+        print("Receive", msg)
+        
 
         piece = {
-            'message': msg["message"] ,
+            'message': msg ,
             'piece': "Rook"
         }
+        # await self.send({
+        #     "type": "websocket.send",
+        #     "text": json.dumps(piece)
+        # })
+        await self.channel_layer.group_send(
+            self.game,
+            {
+                "type": "custom_game",
+                "text": json.dumps(piece)
+             }
+
+        )
+    async def custom_game(self, event):
+        print('message', event)
         await self.send({
             "type": "websocket.send",
-            "text": json.dumps(piece)
+            "text": event['text']
         })
 
     async def websocket_disconnect(self, event):
