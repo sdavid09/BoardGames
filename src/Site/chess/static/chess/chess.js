a5f5766992e919751759506393c6bb23054e2b56
Shobin <shobin.david9@gmail.com>
2020-07-25 12:01:57 -0500

    fixed selection movement based on element

--- src/Site/chess/static/chess/chess.js
+++ src/Site/chess/static/chess/chess.js
@@ -2,7 +2,7 @@ import { Piece, Rook, Knight, Bishop, Queen, King, Pawn } from "./piece.js"
 
 pieces.forEach(element => {
     let boardSquare = document.querySelector(`#${element.square}`)
-    boardSquare.innerHTML=`<span id=${element.id}>${element.icon}</span>`;
+    boardSquare.innerHTML=`<span id=piece-${element.id}>${element.icon}</span>`;
 });
 
 let endpoint = window.location.protocol == 'https:' ? 'wss://' : 'ws://' + window.location.host + window.location.pathname
@@ -35,21 +35,42 @@ table.addEventListener("click", function(event){
         console.log("First Selected")
         if (event.target.textContent){
             firstClick = true;
-
-            selectedFirstPiece = {'piece': event.target.textContent,
-            'square': event.target.getAttribute("id"),
-            "id": event.target.children[0].getAttribute("id")}
+            console.log(event.target)
+            let id;
+            let square;
+            if( event.target.nodeName == "TD"){
+                id = event.target.children[0].getAttribute("id")
+                square = event.target.getAttribute("id")
+            }
+            else if ( event.target.nodeName == "SPAN") {
+                id = event.target.getAttribute("id")
+                square = event.target.parentElement.getAttribute("id")
+            }
+            selectedFirstPiece = {
+                'piece': event.target.textContent,
+                'square': square,
+                "id": id
+            }
         }
     }
+
     else if(event.target.closest('td') && firstClick ){
         console.log("Second Selected")
         firstClick = false;
-        selectedSecondPiece =  {'piece': event.target.textContent,
-        'square': event.target.getAttribute("id"),
-        "id": event.target.children[0].getAttribute("id")}
+        let id;
+            if( event.target.nodeName == "TD"){
+                id = event.target.children[0].getAttribute("id")
+            }
+            else if ( event.target.nodeName == "SPAN") {
+                id = event.target.getAttribute("id")
+            }
+        selectedSecondPiece =  {
+            'piece': event.target.textContent,
+            'square': event.target.getAttribute("id"),
+            "id": id
+        }
         socket.send(JSON.stringify({'firstPiece': selectedFirstPiece, 'secondPiece': selectedSecondPiece}))
     }
-   
 });
 
 
@@ -60,39 +81,3 @@ socket.onerror= function(e){
 socket.onclose= function(e){
     console.log("close", e)
 }
-
-// addFirstPlayerPieces();
-// addSecondPlayerPieces()
-
-// let table = document.querySelector("table")
-// let firstPick = false;
-// let secondPick;
-// let turn = false;
-
-// table.addEventListener("click", function(e){
-//     console.log("CLICKED");
-
-//     if(event.target.matches('td') && !turn && !firstPick && event.target.textContent.length){
-//        let all_classes = event.target.classList
-//        let player_class = all_classes[1]
-//         firstPick = {"icon" : event.target.textContent,
-//                     "class": player_class,
-//                     "element": event.target}
-//         turn = true;
-//     }
-//     else if (event.target.matches('td') && turn && firstPick ) { 
-//         if(event.target.textContent !== firstPick["element"].textContent){
-//             let all_classes = event.target.classList
-//             if( all_classes.length > 1 ){
-//                 event.target.classList.remove(all_classes[1])
-//             }
-//             event.target.textContent = firstPick["icon"];
-//             event.target.classList.add(firstPick["class"])
-//             firstPick["element"].textContent = "";
-//         }
-//         firstPick = false;
-//         turn = false;
-//     }
-// });
-
-// export {table};
