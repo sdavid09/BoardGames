11fae9c378ff520080b7c41248eab01bbc346d3c
Shobin <shobin.david9@gmail.com>
2020-07-25 14:12:05 -0500

    saving movement in db

--- src/Site/chess/consumer.py
+++ src/Site/chess/consumer.py
@@ -1,5 +1,7 @@
 import asyncio
 from asgiref.sync import sync_to_async
+from django.shortcuts import render, get_object_or_404
+from .models import *
 import json
 from channels.consumer import AsyncConsumer
 from channels.generic.websocket import WebsocketConsumer, JsonWebsocketConsumer
@@ -21,16 +23,15 @@ class GameConsumer(AsyncConsumer):
     async def websocket_receive(self, event):
         msg = json.loads(event['text'])
         print("Receive", msg)
-        
+        first_piece = msg['firstPiece']
+        second_piece = msg['secondPiece']
 
         piece = {
             'message': msg ,
             'piece': "Rook"
         }
-        # await self.send({
-        #     "type": "websocket.send",
-        #     "text": json.dumps(piece)
-        # })
+        await self.move_piece(first_piece, second_piece)
+
         await self.channel_layer.group_send(
             self.game,
             {
@@ -39,8 +40,23 @@ class GameConsumer(AsyncConsumer):
              }
 
         )
+    @sync_to_async
+    def move_piece(self, first_piece, second_piece):
+        game = get_object_or_404(Game, id=1)
+        first_piece_square = list(first_piece["square"])
+        second_piece_square = list(second_piece["square"])
+        first_piece_select = BoardSquare.objects.get(board=game.board, row=first_piece_square[1], column=first_piece_square[0])
+        second_piece_select = BoardSquare.objects.get(board=game.board, row=second_piece_square[1], column=second_piece_square[0])
+        first_player_piece = first_piece['id'].split('-')[1]
+        piece = Piece.objects.get(pk=first_player_piece)
+        second_piece_select.piece=piece
+        second_piece_select.save()
+        first_piece_select.piece=None
+        first_piece_select.save()
+
+
     async def custom_game(self, event):
-        print('message', event)
+        # print('message', event)
         await self.send({
             "type": "websocket.send",
             "text": event['text']
